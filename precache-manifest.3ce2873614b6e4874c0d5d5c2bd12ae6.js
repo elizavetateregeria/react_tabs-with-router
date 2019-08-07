7d281432e59d3309199c1e4ef7f2bd5772631353
felsi13 <13felsi@gmail.com>
2019-08-07 15:36:33 +0300

    Updates

---
+++ precache-manifest.3ce2873614b6e4874c0d5d5c2bd12ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bc0eeaf51b5145f086aebb5ce93b69",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "8b271d3fd3949e6c39ab",
+    "url": "/react_tabs-with-router/static/css/main.d0d0622f.chunk.css"
+  },
+  {
+    "revision": "76706dc3523318d70ed9",
+    "url": "/react_tabs-with-router/static/js/2.cc87f309.chunk.js"
+  },
+  {
+    "revision": "8b271d3fd3949e6c39ab",
+    "url": "/react_tabs-with-router/static/js/main.f8826137.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
